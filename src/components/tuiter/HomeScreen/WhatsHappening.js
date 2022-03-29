9344c84840e7046c6d41fd1f86cdd704728b7535
Laura Calderon <calderon.l@northeastern.edu>
2022-03-28 22:45:58 -0400

    whats happening is done

--- src/components/tuiter/HomeScreen/WhatsHappening.js
+++ src/components/tuiter/HomeScreen/WhatsHappening.js
@@ -1,21 +1,36 @@
 import React, {useState} from "react";
-/*import {useDispatch} from "react-redux";*/
+import {useDispatch} from "react-redux";
+import "./index.css";
 
 const WhatsHappening = () => {
-  let [whatsHappening, setWhatsHappening]
-      = useState('');
+  let [whatsHappening, setWhatsHappening] = useState('');
+  const dispatch = useDispatch();
   const tuitClickHandler = () => {
-    console.log(whatsHappening);
+    dispatch({type: 'create-tuit',
+              tuit: whatsHappening});
   }
   return (
     <>
-      <textarea value={whatsHappening}
-               onChange={(event) =>
-        setWhatsHappening(event.target.value)}>
-      </textarea>
-      <button onClick={tuitClickHandler}>
-        Tuit
-      </button>
+      <div className="row">
+            <textarea className="bg-black form-control"
+                value={whatsHappening}
+                placeholder="What's happening?"
+                onChange={(event) =>
+                  setWhatsHappening(event.target.value)
+                }>
+            </textarea>
+          <div className="col-md-10">
+              <i className="fas fa-image pe-1 blue"></i>
+              <i className="fas fa-chart-bar pe-1 blue"></i>
+              <i className="fas fa-smile pe-1 blue"></i>
+              <i className="fas fa-calendar pe-1 blue"></i>
+          </div>
+          <div className="col-md-2">
+              <button className="btn btn-primary rounded-pill float-right" onClick={tuitClickHandler}>
+                Tuit
+              </button>
+          </div>
+       </div>
     </>
   );
 }
